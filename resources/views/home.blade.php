e5560e618d4dff882b149ae02f2cfbed3df0075e
AzfFoysal <abuzehadfoysal@gmail.com>
2021-07-19 10:54:58 +0600

    Update 0.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+            <div class="card border-dark">
+            <center>
+                <div class="card-header card text-white bg-dark">{{ __('Welcome to Your Messanger') }}</div>
+             </center>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,13 +15,22 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
-
+                    <!-- {{ __('You are logged in!') }} -->
+                    <picture>
+                        <source srcset="https://i0.wp.com/justcall.io/blog/wp-content/uploads/2021/05/bulk-sms-banner-image.png?fit=2412%2C988&ssl=1" type="image/svg+xml">
+                        <source
+                            srcset="{{asset('home/Fintechcover.png')}}"
+                            type="image/svg+xml">
+                        <img src="..." class="img-fluid img-thumbnail" alt="...">
+                    </picture>
+                <br>
+                <br>
+                <br>
                     <center>
         
-                      <a class="btn btn-primary"  href="{{ url('/chat') }}">Chat</a>
+                      <a class="btn btn-primary btn-lg"  href="{{ url('/chat') }}">Chat</a>
         
-                   </center>
+                    </center>
                 </div>
             </div>
         </div>
